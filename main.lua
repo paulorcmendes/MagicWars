b251ff40571c0bf5e6b9534cf414585edb8f5011
paulorcmendes <mendesdpn@gmail.com>
2015-06-08 15:08:27 -0300

    ChangeLog:[ADD]animação; [CHANGE]character

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 --##Game Logic
 --#Imports
 local player = require("player")
+local anim8 = require 'anim8'
 character = player.invokeCharacter()
 
 --#Variables
@@ -12,13 +13,13 @@ function  love.load()
 end
 
 --#On every frame
-function love.update()
+function love.update(dt)
 	if gamestate == "intro" then
 
 	elseif gamestate == "menu" then
 
 	elseif gamestate == "onPlay" then
-
+		character[4]:update(dt)
 	elseif gamestate == "onPause" then
 
 	elseif gamestate == "over" then
@@ -29,19 +30,19 @@ function love.update()
 end
 
 --Draw Objects
-function love.draw()
+function love.draw(dt)
 	if gamestate == "intro" then
 
 	elseif gamestate == "menu" then
 
 	elseif gamestate == "onPlay" then
-		
+		character[4]:draw(character[3], 100, 200)
 		love.graphics.print(character[1])
 	elseif gamestate == "onPause" then
 
 	elseif gamestate == "over" then
 
 	elseif gamestate == "credits" then
-		
+
 	end
 end
--- player.lua
+++ player.lua
@@ -2,31 +2,34 @@
 local anim8 = require 'anim8'
 local saveFile = require 'saveFile'
 local M = {}
-local mana = 100
-local robe
+local mana = 5
+local robe = 1
 local character = {mana, robe}
 
 function savePlayer()
-	success = love.filesystem.write( "character.sav", saveFile.tableShow(character, "loadedCharacter"))	
+	success = love.filesystem.write( "character.sav", saveFile.tableShow(character, "loadedCharacter"))
 end
 function getSavedCharacter()
 	chunk = love.filesystem.load( "character.sav" )
 	chunk()
 	return loadedCharacter
 end
-function reloadCharacter()
-	character[1] = character[1]+5
 
-end
-function invokeCharacter()	
+function invokeCharacter()
+	if robe == 1 then
+		image = love.graphics.newImage("white.png")
+		local g = anim8.newGrid(47, 48, image:getWidth(), image:getHeight())
+		character[3] = image
+		character[4] = anim8.newAnimation(g('4-6',1), 0.1)
+	end
 	return character
 end
 
 
 
-savePlayer()
-character = getSavedCharacter()
-reloadCharacter()
+--savePlayer()
+--character = getSavedCharacter()
+
 M.invokeCharacter = invokeCharacter
 
 return M

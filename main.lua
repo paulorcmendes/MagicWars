4a58efc1ef68b064cbb8123fe3909237ac47e738
Paulo Mendes <mendesdpn@gmail.com>
2015-06-13 15:43:36 -0300

    [ADD] Sistema básico de tiro; [ADD] Música agora toca

--- main.lua
+++ main.lua
@@ -2,8 +2,7 @@
 --#Imports
 local player = require("player")
 local anim8 = require 'anim8'
-
-
+local bullets = {}
 --#Variables
 local gamestate  --[[ intro - menu - onPlay - onPause - over - credits ]]
 
@@ -13,7 +12,11 @@ function  love.load()
 	character = player.invokeCharacter()
 	largura = love.graphics.getWidth()
 	altura = love.graphics.getHeight()
+	tempoDeTiro = 0.2
+	ultimoTiro = os.clock()-tempoDeTiro	
 	x = ((largura-47)/2)  
+	y = altura
+	musica = love.audio.newSource("sound.mp3")
 end
 
 --#On every frame
@@ -23,6 +26,7 @@ function love.update(dt)
 	elseif gamestate == "menu" then
 
 	elseif gamestate == "onPlay" then
+		love.audio.play(musica)
 		character.anim:update(dt)
 		if love.keyboard.isDown("right") then
       		x = x + (character.speed * dt)
@@ -30,8 +34,12 @@ function love.update(dt)
    		if love.keyboard.isDown("left") then
       		x = x - (character.speed * dt)
    		end 
+   		if love.keyboard.isDown("a") and podeAtirar() then 
+   			shoot()
+   		end
    		if x<0 then x=0 end  
    		if x>largura-47 then x = largura-47 end
+
 	elseif gamestate == "onPause" then
 
 	elseif gamestate == "over" then
@@ -39,6 +47,7 @@ function love.update(dt)
 	elseif gamestate == "credits" then
 
 	end
+	updateBullets(dt)
 end
 
 --Draw Objects
@@ -48,13 +57,44 @@ function love.draw(dt)
 	elseif gamestate == "menu" then
 
 	elseif gamestate == "onPlay" then
+
 		character.anim:draw(character.image, x, altura - 50)
 		love.graphics.print(character.mana)
+		local i, o
+		for i, o in ipairs(bullets) do
+			love.graphics.circle('fill', o.bx, o.by, 2, 8)
+		end
 	elseif gamestate == "onPause" then
 
 	elseif gamestate == "over" then
 
 	elseif gamestate == "credits" then
 
+	end
+end
+
+function shoot()
+	ultimoTiro = os.clock()
+	table.insert(bullets, {
+			bx = x+47/2,
+			by = y-50,
+			bspeed = 400
+	})
+end
+function podeAtirar()
+	if os.clock()-ultimoTiro<tempoDeTiro then
+		return false
+	else
+	    return true
+	end	
+end
+function updateBullets(dt)
+	-- update bullets:
+	local i, o
+	for i, o in ipairs(bullets) do
+		o.by = o.by - 1* o.bspeed * dt
+		if (o.by < -20) or (o.by > love.graphics.getHeight() + 20) then
+			table.remove(bullets, i)
+		end
 	end
 end
